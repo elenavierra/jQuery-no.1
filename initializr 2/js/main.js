975684e039438b834f1463249d119a4ff1636981
Elena Vierra <elenavierra@utexas.edu>
2016-11-15 18:59:13 -0600

    #6 unhighlight

--- initializr 2/js/main.js
+++ initializr 2/js/main.js
@@ -2,3 +2,7 @@ function highlight(){
   $('h1').text('Hello');
   $(".jumbotron").addClass('highlighted');
 }
+function unhighlight(){
+  $('h1').text('Hello, world!');
+  $(".jumbotron").removeClass('highlighted');
+}
